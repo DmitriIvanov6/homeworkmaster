b8d3eb18b39cd61f672e16eacc86b2ab3ff1d27e
DmitriIvanov6 <ditrimgnom@gmail.com>
2021-03-20 00:43:03 +0300

    Lesson 7

---
+++ src/Lesson7/Plate.java
@@ -0,0 +1,27 @@
+package Lesson7;
+
+public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+
+    public boolean decreaseFood(int n) {
+        if (food >= n) {
+            food -= n;
+            return true;
+        }
+        return false;
+    }
+
+    public void increaseFood(int n) {
+        food += n;
+    }
+
+
+}
